77535a030cef54a2cecd4afad02da17b09c5c7f5
khushbooverma2197 <khushbooverma2197@gmail.com>
2025-11-27 23:47:17 +0530

    Refactor age functions to asynchronous console logs
    Replaced age display and change functions with asynchronous logging.

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-let age = 25;
-function displayAge() {
- console.log(age);
-}
-function changeAge(newAge) {
- age = newAge;           // updates the global variable
- console.log(age);       // print the value after updating
-}
-displayAge();   // prints: 25
-changeAge(30);  // prints: 30
-displayAge();   // prints: 30
+console.log("Begin");
+setTimeout(() => {
+ console.log("Timeout Task");
+}, 0);
+Promise.resolve().then(() => {
+ console.log("Promise Task");
+});
+console.log("End");
