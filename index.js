e4b89921fa527c347623e951bbb02546bb5906ec
khushbooverma2197 <khushbooverma2197@gmail.com>
2025-11-27 23:38:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-let age = 25;
-function displayAge() {
- console.log(age);
-}
-function changeAge(newAge) {
- age = newAge;           // updates the global variable
- console.log(age);       // print the value after updating
-}
-displayAge();   // prints: 25
-changeAge(30);  // prints: 30
-displayAge();   // prints: 30
+console.log("Task 1");
+console.log("Task 2");
+console.log("Task 3");
